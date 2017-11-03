7ea286dd231dc2f0f447a9805802acdcce117098
Nicolas M <nicolas.mariangel@usach.cl>
2017-11-03 16:30:52 -0300

    Finish testing time with lab PCs

--- testTime.sh
+++ testTime.sh
@@ -1,10 +1,10 @@
 readonly limit=10
-{ for h in {1..14}; do time for i in {1..limit}; do ./wave -N 128 -T 2000 -H $h -f nada.txt -t 2000; done; done; } 2> times/time-128-2000.txt
-{ for h in {1..14}; do time for i in {1..limit}; do ./wave -N 128 -T 4000 -H $h -f nada.txt -t 4000; done; done; } 2> times/time-128-4000.txt
-{ for h in {1..14}; do time for i in {1..limit}; do ./wave -N 128 -T 8000 -H $h -f nada.txt -t 8000; done; done; } 2> times/time-128-8000.txt
-{ for h in {1..14}; do time for i in {1..limit}; do ./wave -N 256 -T 2000 -H $h -f nada.txt -t 2000; done; done; } 2> times/time-256-2000.txt
-{ for h in {1..14}; do time for i in {1..limit}; do ./wave -N 256 -T 4000 -H $h -f nada.txt -t 4000; done; done; } 2> times/time-256-4000.txt
-{ for h in {1..14}; do time for i in {1..limit}; do ./wave -N 256 -T 8000 -H $h -f nada.txt -t 8000; done; done; } 2> times/time-256-8000.txt
-{ for h in {1..14}; do time for i in {1..limit}; do ./wave -N 512 -T 2000 -H $h -f nada.txt -t 2000; done; done; } 2> times/time-512-2000.txt
-{ for h in {1..14}; do time for i in {1..limit}; do ./wave -N 512 -T 4000 -H $h -f nada.txt -t 4000; done; done; } 2> times/time-512-4000.txt
-{ for h in {1..14}; do time for i in {1..limit}; do ./wave -N 512 -T 8000 -H $h -f nada.txt -t 8000; done; done; } 2> times/time-512-8000.txt
+{ for h in {1..14}; do time for i in {1..10}; do ./wave -N 128 -T 2000 -H $h -f nada.txt -t 2000; done; done; } 2> times/time-128-2000.txt
+{ for h in {1..14}; do time for i in {1..10}; do ./wave -N 128 -T 4000 -H $h -f nada.txt -t 4000; done; done; } 2> times/time-128-4000.txt
+{ for h in {1..14}; do time for i in {1..10}; do ./wave -N 128 -T 8000 -H $h -f nada.txt -t 8000; done; done; } 2> times/time-128-8000.txt
+{ for h in {1..14}; do time for i in {1..10}; do ./wave -N 256 -T 2000 -H $h -f nada.txt -t 2000; done; done; } 2> times/time-256-2000.txt
+{ for h in {1..14}; do time for i in {1..10}; do ./wave -N 256 -T 4000 -H $h -f nada.txt -t 4000; done; done; } 2> times/time-256-4000.txt
+{ for h in {1..14}; do time for i in {1..10}; do ./wave -N 256 -T 8000 -H $h -f nada.txt -t 8000; done; done; } 2> times/time-256-8000.txt
+{ for h in {1..14}; do time for i in {1..10}; do ./wave -N 512 -T 2000 -H $h -f nada.txt -t 2000; done; done; } 2> times/time-512-2000.txt
+{ for h in {1..14}; do time for i in {1..10}; do ./wave -N 512 -T 4000 -H $h -f nada.txt -t 4000; done; done; } 2> times/time-512-4000.txt
+{ for h in {1..14}; do time for i in {1..10}; do ./wave -N 512 -T 8000 -H $h -f nada.txt -t 8000; done; done; } 2> times/time-512-8000.txt
--- timeTranspose.c
+++ timeTranspose.c
@@ -3,18 +3,22 @@
 
 int main() {
 	int i, j, k, H = 14, N = 128, T = 2000;
-	float iterations = 1;
+	float iterations = 10;
 	int minutes;
 	float seconds, realTime, totalTime = 0;
+	float time1;
 	FILE* input;
 	FILE* output;
+	FILE* speedup;
+	FILE* efficiency;
 
-	output = fopen("times.txt", "w");
+	output = fopen("times.csv", "w");
+	speedup = fopen("speedup.csv", "w");
+	efficiency = fopen("efficiency.csv", "w");
 
-	for(j = 0; j <= 2; j++) {
+	for(j = 0; j <= 1; j++) {
 		if(j == 0) N = 128;
 		if(j == 1) N = 256;
-		if(j == 2) N = 512;
 		for(k = 0; k <= 2; k++) {
 			if(k == 0) T = 2000;
 			if(k == 1) T = 4000;
@@ -23,26 +27,39 @@ int main() {
 			char filename[100];
 			sprintf(filename, "times/time-%d-%d.txt", N, T);
 
-			fprintf(output, "N=%d,T=%d", N, T);
+			fprintf(output, "%d,%d", N, T);
+			fprintf(speedup, "%d,%d", N, T);
+			fprintf(efficiency, "%d,%d", N, T);
 			input = fopen(filename, "r");
 			for(i = 0; i < H; i++) {
 				fscanf(input, " real %dm%fs", &minutes, &seconds);
 				realTime = (float)minutes * 60.0 +  seconds;
 				totalTime += realTime; /* Total execution time */
 				realTime /= iterations;
+
+				if(i == 0) {
+					time1 = realTime;
+				}
+	
 				fprintf(output, ",%f", realTime);
+				fprintf(speedup, ",%f", time1/realTime);
+				fprintf(efficiency, ",%f", time1/(realTime * (float)(i+1)));
 
 				// Read other unused data.
 				fscanf(input, " user %dm%fs", &minutes, &seconds);
 				fscanf(input, " sys %dm%fs", &minutes, &seconds);
 			}
 			fprintf(output, "\n");
+			fprintf(speedup, "\n");
+			fprintf(efficiency, "\n");
 		}
 	}
 
 	printf("Total script execution time = %f minutes\n", totalTime/60.0);
 
 	fclose(output);
+	fclose(speedup);
+	fclose(efficiency);
 
 
 	return 0;
